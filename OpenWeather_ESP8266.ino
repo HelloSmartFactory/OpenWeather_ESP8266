eb25e70a366ab9690b027bfd36b1fac960580e5b
HelloSmartFactory <49864006+HelloSmartFactory@users.noreply.github.com>
2021-09-08 15:06:28 +0900

    Update OpenWeather_ESP8266.ino

--- OpenWeather_ESP8266.ino
+++ OpenWeather_ESP8266.ino
@@ -1,6 +1,6 @@
 #include <ESP8266WiFi.h>
 #include <ESP8266HTTPClient.h>
-#include <ArduinoJson.h>
+#include <ArduinoJson.h> //v6を使用しています
 #define ARDUINOJSON_DECODE_UNICODE 1
 
 ////////////////////////////////////
